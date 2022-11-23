795384fe4a8ac198449f4ecec5b1c2901549b198
Vlad-lap95 <Vlad.Lapeonok@gmail.com>
2022-11-23 14:45:07 +0300

    create file les4

---
+++ les4.py
@@ -0,0 +1,15 @@
+
+import math
+print('Ответ к примеру 1:', 1**100)                          # Primer1
+print('Ответ к примеру 2:', 2**16)                           # Primer2
+print('Ответ к примеру 3:', 2-1000)                          # Primer3
+print('Ответ к примеру 4:', 2+2*2)                           # Primer4
+print('Ответ к примеру 5:', 60/3)                            # Primer5
+print('Ответ к примеру 6:', math.sqrt(144))                  # Primer6
+print('Ответ к примеру 7:', 2+5*14)                          # Primer7
+#Primer8 66-x=11
+x=66-11
+print('Ответ к примеру 8:', x)
+#Primer9 math.sqrt(x)+8=16
+x=(16-8)**2
+print('Ответ к примеру 9:', x)
